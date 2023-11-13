6296b03e7f4b0c09918f67deca1e78af1e0d13b1
xjama <Jamshidbekollanazarov@gmail.com>
2023-11-13 01:34:13 -0600

    Most replayed, Creating a Factory, Creating a layout & sections

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,34 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing{
-        public static function all() {
-            return [
-                    [
-                        'id' => 0,
-                        'title' => 'Listing one',
-                        'description' => 'lorem ipsum dolor sit amet lore mauris vel'
-                    ],
-                    [
-                        'id' => 1,
-                        'title' => 'Listing two',
-                        'description' => 'lorem ipsum dolor sit amet lore mauris vel'
-                    ],
-                    [
-                        'id' => 2,
-                        'title' => 'Listing three',
-                        'description' => 'lorem ipsum dolor sit amet lore mauris vel'
-                    ]    
-                ];
-            }
-        
-        public static function find($id) {
-            $listings = self::all();
-            foreach ($listings as $listing) {
-                if ($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-            return ["title" => "Not Found", "description" => ""];
-        }
-    }
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
